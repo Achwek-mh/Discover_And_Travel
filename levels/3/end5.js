45e01128c81f5f570fbfebc7c9ecfc4cc5848535
Achwek-mh <achwek.mhenni@ensi-uma.tn>
2022-05-09 10:05:21 +0100

    Final commit

---
+++ levels/3/end5.js
@@ -0,0 +1,20 @@
+let clickend=0;
+ 
+ const exit = document.getElementById("dismisss");
+exit.addEventListener('click',  ()  =>  {
+   
+    if (exit.innerHTML=="Try again" ){
+    document.getElementById('finish').style.display = 'none';
+  
+    location.reload();}
+     else 
+     window.location.pathname='play.html';
+     clickend=1;
+     stopp=1;
+
+}) 
+const next=document.getElementById("nexxt");
+next.addEventListener('click',() => {
+    document.getElementById('finish').style.display = 'none';
+    window.location.pathname = 'levels/4/maze6.html';
+})
